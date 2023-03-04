3af0b6050ebafced8286701d15d17cd66672ccb5
Stelios Miskedakis <123480703+steliosmisk@users.noreply.github.com>
2023-03-04 08:39:41 +0200

    Update average.py

--- average.py
+++ average.py
@@ -1,17 +1,17 @@
-print("--Welcome to Average Calculator--")
+print("---- Welcome to Average Calculator ----")
 print("[1] Start")
 print("[2] Exit")
 
-user_choice = int(input("Enter (1/2): "))
+user_choice = int(input("Enter your choice (1/2): "))
 while user_choice == 1:
     try:
         subjects_list = []
-        user_subjects = int(input(">> Enter, how many subjects you want: "))
-        for subject in range(user_subjects):
-            user_value = float(input(f">> {subject} Enter a Value: "))
+        user_subjects = int(input("Enter the number of subjects: "))
+            for subject in range(1, user_subjects+1):
+            user_value = float(input(f"Enter value {subject}: "))
             subjects_list.append(user_value)
-        print(">> Your values is:", subjects_list, "now I'll calculate this list.")
-        print(">> Results:", sum(subjects_list) / user_subjects)
+        print("Your values are:", subjects_list)
+        print("Average of the values:", sum(subjects_list) / user_subjects)
         break
-    except ValueError:
-        print("Enter a valid value!")
+except ValueError:
+print("Invalid input! Please enter a valid value.")
